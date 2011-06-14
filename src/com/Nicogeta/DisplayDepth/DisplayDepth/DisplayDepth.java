071e87f921bfaac231c33080b5a6064139aa5985
Nicogeta <nicogeta@sfr.fr>
2011-06-14 08:02:11 +0200

    minor modifs

--- src/com/Nicogeta/DisplayDepth/DisplayDepth/DisplayDepth.java
+++ src/com/Nicogeta/DisplayDepth/DisplayDepth/DisplayDepth.java
@@ -1,130 +1,122 @@
-    package com.Nicogeta.DisplayDepth.DisplayDepth;
-     
-    import java.io.File;
-    import java.io.FileInputStream;
-    import java.io.FileNotFoundException;
-    import java.io.FileOutputStream;
-    import java.io.IOException;
-    import java.util.Properties;
-    import java.util.logging.Logger;
-     
+package com.Nicogeta.DisplayDepth.DisplayDepth;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-    import org.bukkit.event.Event;
-    import org.bukkit.event.Event.Priority;
-    import org.bukkit.event.player.PlayerLoginEvent;
-    import org.bukkit.plugin.Plugin;
-    import org.bukkit.plugin.PluginManager;
-    import org.bukkit.plugin.java.JavaPlugin;
-     
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
+//import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
-    import com.nijiko.permissions.PermissionHandler;
+
+import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
-     
-    public class DisplayDepth extends JavaPlugin {
-     
-            private static final Logger log = Logger.getLogger("Minecraft");
-            public static PermissionHandler Permissions;
-            private final DisplayDepthPlayerListener playerListener = new DisplayDepthPlayerListener(this);
-     
-            public int theItemID;
-            String theItemToUse;
-            static String mainDirectory = "plugins/DisplayDepth";
-            static File ConfigCreate = new File(mainDirectory + File.separator + "config.properties");
-            static Properties prop = new Properties();
-     
-     
-            public void loadProcedure() {
-                    FileInputStream inFile = null;
-     
-                    try {
-                            inFile = new FileInputStream(ConfigCreate);
-                            prop.load(inFile);
-                            inFile.close();
-                    } catch(FileNotFoundException e) {
-                            throw new RuntimeException("Config file not found.", e);
-                    } catch (IOException e){
-                            throw new RuntimeException("Failed to load config file.", e);
-                    }
-     
-                    String theItem = prop.getProperty("itemToUse");
-                    try {
-                            theItemID = Integer.parseInt(theItem);
-                    } catch (NumberFormatException e) {
-                            throw new RuntimeException("Failed to parse item ID.", e);
-                    }
-            }
-     
-     
-            public void onEnable() {
-                    new File(mainDirectory).mkdir();
-                    if(!ConfigCreate.exists()) {
-                            try {
-                                    ConfigCreate.createNewFile();
-                                    FileOutputStream out = new FileOutputStream(ConfigCreate);
-                                    prop.put("itemToUse", "347");
-                                    prop.store(out, "DisplayDepth config, please put the Id of the item you want to use");
-                                    out.flush();
-                                    out.close();
-                            } catch (IOException e) {
-                                    throw new RuntimeException("Failed to create default config file.", e);
-                            }
-                    }
-                    loadProcedure();
-                    PluginManager pm = getServer().getPluginManager();
-                    pm.registerEvent(Event.Type.PLAYER_INTERACT, playerListener, Priority.Normal, this);
-                    setupPermissions();
-                    log.info("DisplayDepth ENABLE (by Nicogeta)");
-                    log.info("DisplayDepth ver 0.2");
-            }
-     
-            private void setupPermissions() {
-                    Plugin test = this.getServer().getPluginManager().getPlugin("Permissions");
-                    if (DisplayDepth.Permissions == null)
-                    {
-                            if (test != null) {
-                                    DisplayDepth.Permissions = ((Permissions)test).getHandler();
-                                    log.info("DisplayDepth has detected Permissions!");
-                            } else {
-                                    log.info("DisplayDepth has not detected Permissions.");
-                            }
-                    }
-            }
-     
-            public void onDisable() {
-                    log.info("DisplayDepth DISABLE");
-            }
-            
-            
-        	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        		Player player;
-        		if (command.getName().equalsIgnoreCase("rdd")) {
-        		if(sender instanceof Player) {
-        			player = (Player) sender;
-        			if ((DisplayDepth.Permissions == null && player.isOp()) 
-        				|| (DisplayDepth.Permissions != null && DisplayDepth.Permissions.has(player, "DisplayDepth.reload"))) {
-        				
-        				if(args.length == 0) {
-        					loadProcedure();
-        					player.sendMessage(ChatColor.GREEN + "INFO: Changes applied");
-        				} else {
-        					player.sendMessage(ChatColor.RED + "INFO: You have to type /rdd");
-        				}
-        			} else {
-        				player.sendMessage("You don't have the permission to do that !");
-        			}
-        		}
-        		}
-				return true;	
-        	}
-     
-     
-     
-            public void recordEvent(PlayerLoginEvent event) {
-                    // TODO Auto-generated method stub
-     
-            }
-     
-    }
+
+public class DisplayDepth extends JavaPlugin {
+
+	private static final Logger log = Logger.getLogger("Minecraft");
+	public static PermissionHandler Permissions;
+	private final DisplayDepthPlayerListener playerListener = new DisplayDepthPlayerListener(this);
+
+	public int theItemID;
+	String theItemToUse;
+	static String mainDirectory = "plugins/DisplayDepth";
+	static File ConfigCreate = new File(mainDirectory + File.separator + "config.properties");
+	static Properties prop = new Properties();
+
+
+	public void loadProcedure() {
+		FileInputStream inFile = null;
+
+		try {
+			inFile = new FileInputStream(ConfigCreate);
+			prop.load(inFile);
+			inFile.close();
+		} catch(FileNotFoundException e) {
+			throw new RuntimeException("Config file not found.", e);
+		} catch (IOException e){
+			throw new RuntimeException("Failed to load config file.", e);
+		}
+
+		String theItem = prop.getProperty("itemToUse");
+		try {
+			theItemID = Integer.parseInt(theItem);
+		} catch (NumberFormatException e) {
+			throw new RuntimeException("Failed to parse item ID.", e);
+		}
+	}
+
+
+	public void onEnable() {
+		new File(mainDirectory).mkdir();
+		if(!ConfigCreate.exists()) {
+			try {
+				ConfigCreate.createNewFile();
+				FileOutputStream out = new FileOutputStream(ConfigCreate);
+				prop.put("itemToUse", "347");
+				prop.store(out, "DisplayDepth config, please put the Id of the item you want to use");
+				out.flush();
+				out.close();
+			} catch (IOException e) {
+				throw new RuntimeException("Failed to create default config file.", e);
+			}
+		}
+		loadProcedure();
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvent(Event.Type.PLAYER_INTERACT, playerListener, Priority.Normal, this);
+		setupPermissions();
+		log.info("DisplayDepth ENABLE (by Nicogeta)");
+		log.info("DisplayDepth ver 0.2");
+	}
+
+	private void setupPermissions() {
+		Plugin test = this.getServer().getPluginManager().getPlugin("Permissions");
+		if (DisplayDepth.Permissions == null)
+		{
+			if (test != null) {
+				DisplayDepth.Permissions = ((Permissions)test).getHandler();
+				log.info("DisplayDepth has detected Permissions!");
+			} else {
+				log.info("DisplayDepth has not detected Permissions.");
+			}
+		}
+	}
+
+	public void onDisable() {
+		log.info("DisplayDepth DISABLE");
+	}
+
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		Player player;
+		if (command.getName().equalsIgnoreCase("rdd")) {
+			if(sender instanceof Player) {
+				player = (Player) sender;
+				if ((DisplayDepth.Permissions == null && player.isOp()) 
+						|| (DisplayDepth.Permissions != null && DisplayDepth.Permissions.has(player, "DisplayDepth.reload"))) {
+
+					if(args.length == 0) {
+						loadProcedure();
+						player.sendMessage(ChatColor.GREEN + "INFO: Changes applied");
+					} else {
+						player.sendMessage(ChatColor.RED + "INFO: You have to type /rdd");
+					}
+				} else {
+					player.sendMessage("You don't have the permission to do that !");
+				}
+			}
+		}
+		return true;	
+	}
+}
--- src/com/Nicogeta/DisplayDepth/DisplayDepth/DisplayDepthPlayerListener.java
+++ src/com/Nicogeta/DisplayDepth/DisplayDepth/DisplayDepthPlayerListener.java
@@ -2,41 +2,38 @@
 package com.Nicogeta.DisplayDepth.DisplayDepth;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerListener;
-//import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-@SuppressWarnings("unused")
+
 
 public class DisplayDepthPlayerListener extends PlayerListener {
-	
+
 	final DisplayDepth plugin;
-	
+
 	public DisplayDepthPlayerListener (DisplayDepth instance) {
 		plugin = instance;
 	}
-	
+
 	public void onPlayerInteract (PlayerInteractEvent event) {
 		Player player = event.getPlayer();
 		int location = event.getPlayer().getLocation().getBlockY();
 		Action action = event.getAction();
-    	boolean display = false;
-    	if((DisplayDepth.Permissions == null && player.isOnline()))
-    		display = true;
-    	else if(DisplayDepth.Permissions != null && DisplayDepth.Permissions.has(player, "DisplayDepth.display"))
-    		display = true;
-	
-	
-    	if (display) {
-    		if(action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK ) {
-    			if (player.getItemInHand().getTypeId() == plugin.theItemID) {
-    				player.sendMessage(ChatColor.GREEN + "INFO: " + "You're " + location + " over 0"); 
-    			}
-    		}
-    	}
-    }		
+		boolean display = false;
+		if((DisplayDepth.Permissions == null && player.isOnline()))
+			display = true;
+		else if(DisplayDepth.Permissions != null && DisplayDepth.Permissions.has(player, "DisplayDepth.display"))
+			display = true;
+
+
+		if (display) {
+			if(action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK ) {
+				if (player.getItemInHand().getTypeId() == plugin.theItemID) {
+					player.sendMessage(ChatColor.GREEN + "INFO: " + "You're " + location + " over 0"); 
+				}
+			}
+		}
+	}		
 }
